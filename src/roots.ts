c1918ef47efa52fa486d6b5a9d28ada4a7dda1c5
Ben Francis <siscorule@gmail.com>
2023-08-30 15:32:04 +0100

    Updates to manifest

--- src/roots.ts
+++ src/roots.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://62.173.42.228:9092/v1/";
-export const BASE_URL = process.env.NODE_ENV === "production" ? "https://evtech.algebraplus.ng/v1/" : "http://192.168.1.11:9092/v1/";
+export const BASE_URL = process.env.NODE_ENV === "production" ? "https://evtechapi.algebraplus.ng/v1/" : "http://192.168.1.11:9092/v1/";
