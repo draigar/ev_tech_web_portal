b5397766279e5045e4a3c4e36ec5fe2a2c5e96c4
Francis Benjamin <siscorule@gmail.com>
2024-02-27 11:09:29 +0100

    New deployed service

--- src/roots.ts
+++ src/roots.ts
@@ -1,3 +1,3 @@
 
-export const BASE_URL = process.env.NODE_ENV === "production" ? "https://evtechapi.algebraplus.ng/v1/" : "http://192.168.1.11:9092/v1/";
+export const BASE_URL = process.env.NODE_ENV === "production" ? "https://evtech-render-admin.onrender.com/v1/" : "http://192.168.1.11:9092/v1/";
 // export const BASE_URL = process.env.NODE_ENV === "production" ? "https://evtechapi.algebraplus.ng/v1/" : "http://62.173.42.228:9092/v1/";
